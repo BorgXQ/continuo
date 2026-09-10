9979fba30a110d8b640ece016453cadaa0085a2c
BorgXQ <122294447+BorgXQ@users.noreply.github.com>
2026-09-10 14:41:20 +0800

    Add load audio logic

---
+++ src/load_audio.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import librosa
+
+
+def load_audio(
+    path: str | Path,
+    target_sr: int = 44_100,
+):
+    path = Path(path)
+
+    if not path.exists():
+        raise FileNotFoundError(path)
+
+    playback_audio, sr = librosa.load(
+        path,
+        sr=target_sr,
+        mono=False,
+    )
+
+    if playback_audio.ndim == 2:
+        analysis_audio = librosa.to_mono(playback_audio)
+    else:
+        analysis_audio = playback_audio.copy()
+
+    return playback_audio, analysis_audio, sr
